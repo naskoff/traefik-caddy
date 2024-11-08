e5adc9bb7d89e092fa83a632785ebb3dcc16c02f
Zlatin Hristov <zlatin.hristov@softavis.com>
2024-11-08 14:41:11 +0200

    Add traefik + caddy proxy

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomepageController extends AbstractController
 {
     #[Route('/home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return new Response('Ok');
+        return new Response(print_r($request->headers->all(), true));
     }
 }
